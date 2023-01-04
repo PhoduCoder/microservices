21bd80af091afe01f7b6f177afb2e69859beafff
EC2 Default User <ec2-user@ip-172-31-81-168.ec2.internal>
2023-01-04 19:45:47 +0000

    Install tool for utility tools like kubectl and awscli update

---
+++ scripts/install-tools.sh
@@ -0,0 +1,33 @@
+sudo curl --silent --location -o /usr/local/bin/kubectl \
+   https://s3.us-west-2.amazonaws.com/amazon-eks/1.21.5/2022-01-21/bin/linux/amd64/kubectl
+
+sudo chmod +x /usr/local/bin/kubectl
+
+#Install awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+#Install jq and other tools
+
+sudo yum -y install jq gettext bash-completion moreutils
+
+echo 'yq() {
+  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
+}' | tee -a ~/.bashrc && source ~/.bashrc
+
+#verify install
+for command in kubectl jq envsubst aws
+  do
+    which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
+  done
+
+#kubectl bash-completion
+kubectl completion bash >>  ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+
+#export bash completion
+echo 'export LBC_VERSION="v2.4.1"' >>  ~/.bash_profile
+echo 'export LBC_CHART_VERSION="1.4.1"' >>  ~/.bash_profile
+.  ~/.bash_profile
